0549420cb43924b8e734bf577118f9e2236df6c2
MenassieErmias <ermiasmenase@gmail.com>
2023-09-01 11:45:44 +0300

    basic react caluculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import NumberInput from "./components/NumberInput";
+
+import { useState } from "react";
 
 function App() {
+  // STATE
+  const [display, setDisplay]=useState("")
+  // FUNCTIONS
+  const createDigits = () =>{
+    const digits = [];
+    for(let i = 0 ; i <= 9; i++){
+      digits.push(<NumberInput digit={i} display={display} setDisplay={setDisplay} />)
+    }
+    return digits
+  }
+
+  const clear = () =>{
+    setDisplay('')
+  }
+
+  const backspace = () => {
+    setDisplay(prev=> prev.slice(0, prev.length-1))
+  }
+
+  const handleClick = (e) => {
+    setDisplay(prev=> prev+e.target.value)
+  }
+
+  const handleEquals = () => {
+    const trimmedEval = display.split(" ").join('');
+    setDisplay(eval(trimmedEval))
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App__h1">Simple javascript calculator</h1>
+      <div className="display">
+        {display}
+      </div>
+      <div className="input-container">
+          {createDigits()}
+          <button className="button" onClick={clear}>CE</button>
+          <button className="button" onClick={backspace}>&#x2190;</button>
+          <button className="button" onClick={handleClick} value={" + "}>+</button>
+          <button className="button" onClick={handleClick} value={" - "}>-</button>
+          <button className="button" onClick={handleClick} value={" * "}>*</button>
+          <button className="button" onClick={handleClick} value={" / "}>/</button>
+          <button className="equals-button button" onClick={handleEquals}> = </button>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/NumberInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NumberInput = ({digit, display, setDisplay}) => {
+    function handleClick(){
+        setDisplay(prev=>prev+digit)
+    }
+    
+    return (
+        <button className='number-input button' onClick={handleClick}>{digit}</button>
+    )
+}
+
+export default NumberInput
